2f2a94a2071c9256fd42e2effc11a28db2bf7753
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2021-04-27 21:15:27 +0200

    Add files via upload

---
+++ Async_Await/Async_Await/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    class Program
+    {
+        static private void ScaricaPaginaWeb(string url)
+        {
+            WebClient web = new WebClient();
+            string page = web.DownloadString(url);
+            Console.WriteLine("Sincrono: Fine");
+        }
+        static private async void ScaricaPaginaWebAsync(string url)
+        {
+            WebClient web = new WebClient();
+            string page = await web.DownloadStringTaskAsync(url);
+            //Console.WriteLine(page);
+            Console.WriteLine("Async: Fine");
+        }
+        static void Main(string[] args)
+        {        
+            ScaricaPaginaWebAsync("http://google.com");
+            ScaricaPaginaWeb("http://google.com");
+            Console.ReadKey();
+        }
+    }
+}
